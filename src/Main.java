7577a1654a868e058b7ba9afbeef117496c34e38
Robyn Doyle <robyn.doylee@hotmail.com>
2021-12-07 13:09:18 +0100

    changes to sout

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("No longer hello. Goodbnye world.");
+        System.out.println("No longer hello. GodBUY world.");
     }
 }
