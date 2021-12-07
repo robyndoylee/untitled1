fc3cce0e32d29259158275b7b30cd6d2f5bb2100
Robyn Doyle <robyn.doylee@hotmail.com>
2021-12-07 13:21:49 +0100

    chaging sout to new feature

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("No longer hello. Goodbye world.");
         System.out.println("6 & 7 combined");
+        System.out.println("this is a new feature");
     }
 }
